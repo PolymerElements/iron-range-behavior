43335be3b9f04c4fd9082e92c968a56e13403ced
Peter Burns <rictic@google.com>
2017-08-22 01:55:50 -0700

    Automatic polymer-modulizer conversion of 521b30fdd20f1e8118f141ed46d90655ac5f124a

---
+++ test/x-progressbar.js
@@ -0,0 +1,7 @@
+import { IronRangeBehavior } from '../iron-range-behavior.js';
+import { Polymer } from '../../polymer/lib/legacy/polymer-fn.js';
+Polymer({
+  is: 'x-progressbar',
+
+  behaviors: [IronRangeBehavior]
+});
